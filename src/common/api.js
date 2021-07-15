5b9d2b3b437fb8c827acc6aaadefc199c04943a5
Etesh <eteshsinha@gmail.com>
2021-07-15 21:04:40 +0530

    coomit the code

---
+++ src/common/api.js
@@ -0,0 +1,6 @@
+import { GooglePlacesKey } from "./apiKey";
+
+const rootURLPlaces = `https://maps.googleapis.com/maps/api/place/${GooglePlacesKey}`;
+const rootURLGeoCodes = `http://maps.googleapis.com/maps/api/geocode/json?latlng=`;
+
+export {rootURLPlaces, rootURLGeoCodes}
